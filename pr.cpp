5a47fe0415704cf3fc42150e25b4daf8fe8d7089
vidhikabathani <vidhikabathani110@gmail.com>
2024-02-02 07:45:32 +0530

    first commit

---
+++ pr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    int z=x+y;
+
+    cout <<"Enter X:"
+    cin >>x
+
+    cout <<"Enter Y:"
+    cin >>y
+
+    cout<<"Sum:"<<z;
+}
